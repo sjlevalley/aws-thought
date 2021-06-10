587e2e411245215aa57588ef67d61578fc16ecd6
Steve LeValley <slevalley@gmail.com>
2021-06-10 13:06:36 -0700

    Profile now rendering pics with thoughts

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -31,12 +31,13 @@ router.get('/users/:username', (req, res) => {
     console.log("Get Thoughts for username route received");
     const params = {
         TableName: table,
-        ProjectionExpression: "#th, #ca",
+        ProjectionExpression: "#th, #ca, #un, #img",
         KeyConditionExpression: "#un = :user",
         ExpressionAttributeNames: {
             "#un": "username",
             "#ca": "createdAt",
-            "#th": "thought"
+            "#th": "thought",
+            "#img": "image"
         },
         ExpressionAttributeValues: {
             ":user": req.params.username
@@ -60,7 +61,8 @@ router.post('/users', (req, res) => {
         Item: {
             "username": req.body.username,
             "createdAt": Date.now(),
-            "thought": req.body.thought
+            "thought": req.body.thought,
+            "image": req.body.image
         }
     };
     dynamodb.put(params, (err, data) => {
